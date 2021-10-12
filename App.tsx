d6aa2f8e1e11ffa4c4fbce00b9260c21ce096a48
Attasiem Kingsley <attasiemj@gmail.com>
2021-10-12 11:21:18 +0100

    fix: resolve navigation and modules

--- App.tsx
+++ App.tsx
@@ -1,70 +1,29 @@
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
  * @format
+ * @flow strict-local
  */
 
-import React from 'react';
-import {
-  Link,
-  Text,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  VStack,
-  Code,
-} from 'native-base';
-import NativeBaseIcon from './src/components/NativeBaseIcon';
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === 'light' ? true : false}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
-const App = () => {
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: 'blueGray.900' }}
-        _light={{ bg: 'blueGray.50' }}
-        px={4}
-        flex={1}>
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.tsx</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={'xl'}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
-};
-export default App;
+ import React from 'react';
+ import {
+   SafeAreaView,
+   View,
+   Text
+ } from 'react-native';
+ import { Provider } from 'react-redux';
+ import Navigation from './src/navigation';
+import { store } from './src/store';
+ 
+ 
+ const App = () => {
+   return (
+     <Provider store={store}>
+       <Navigation />
+     </Provider>
+   );
+ };
+ 
+ export default App;
